f13b1e4710341765fb75fca442b30e9b14beb451
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2024-08-22 06:11:53 -0700

    changed the s3 upload path to include project reference and github sha

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,17 @@ ls -la
 echo "Deploying to environment: $INPUT_ENVIRONMENT"
 
 # 1. Compress the current directory, excluding .git and the script itself
-tar --exclude='./.git' --exclude='./entrypoint.sh' -czf /tmp/project.tar.gz .
-echo "Created project.tar.gz in /tmp"
+tar --exclude='./.git' --exclude='./entrypoint.sh' -czf /tmp/repo.tar.gz .
+echo "Created repo.tar.gz in /tmp"
 
 # Check if the tar file was created successfully
-if [ -f "/tmp/project.tar.gz" ]; then
-    echo "Successfully created project.tar.gz in /tmp"
+if [ -f "/tmp/repo.tar.gz" ]; then
+    echo "Successfully created repo.tar.gz in /tmp"
     # Optional: You can add a size check here
-    tar_size=$(du -h /tmp/project.tar.gz | cut -f1)
-    echo "Size of project.tar.gz: $tar_size"
+    tar_size=$(du -h /tmp/repo.tar.gz | cut -f1)
+    echo "Size of repo.tar.gz: $tar_size"
 else
-    echo "Error: Failed to create project.tar.gz"
+    echo "Error: Failed to create repo.tar.gz"
     exit 1
 fi
 
@@ -29,15 +29,18 @@ current_datetime=$(date +'%Y%m%d-%H%M%S')
 echo "Current date and time: $current_datetime"
 
 # 3. Get a presigned URL using the API
+object_key="/$INPUT_PROJECT_REF/$GITHUB_SHA/repo.tar.gz"
 response=$(curl -X POST -H "X-API-KEY: $INPUT_API_TOKEN" \
            -H "Content-Type: application/json" \
-           -d "{\"object_key\": \"project-$current_datetime.tar.gz\", \"bucket_name\": \"protoml-projects-prod\"}" \
+           -d "{\"object_key\": \"$object_key\", \"bucket_name\": \"protoml-projects-prod\"}" \
            https://api.agentifyme.ai/api/generate-presigned-url)
 upload_url=$(echo $response | jq -r .data.upload_url)
 
 # 4. Upload the tar file to the presigned URL
-curl -X PUT -T /tmp/project.tar.gz "$upload_url"
-echo "Uploaded project.tar.gz to presigned URL"
+curl -X PUT -T /tmp/repo.tar.gz "$upload_url"
+echo "File uploaded to: protoml-projects-prod$object_key"
+
+# 5. Print the path to which the file was uploaded
 
 # # 5. Make an API call to deploy (POST request)
 # deploy_response=$(curl -X POST -H "X-API-KEY: $INPUT_API_TOKEN" \
