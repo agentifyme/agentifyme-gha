e28af5640f05baa895b89931b5afbd74e8f680ce
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2024-08-25 18:12:53 -0700

    fixing bug in auth cli command.

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,20 +36,14 @@ df -h / | tail -n 1 | awk '{print $2 " total, " $3 " used, " $4 " available"}'
 
 echo "===== End of System Information ====="
 
-# Prerequisites
-ldd --version
-curl --version
-jq --version
 
 # 1. Download the agentifyme CLI
 curl -sSL https://agentifyme.ai/install.sh | bash
 export PATH=$PATH:$HOME/.agentifyme/bin
-echo "Agentifyme CLI installed successfully"
 echo `agentifyme version`
 
 # 2. Check if you can authenticate with the API
-agentifyme auth --api-key $INPUT_API_TOKEN
-echo "Successfully authenticated with the API"
+agentifyme auth login --api-key $INPUT_API_TOKEN
 
 # 3. Execute the deploy command
 agenitfyme deploy
